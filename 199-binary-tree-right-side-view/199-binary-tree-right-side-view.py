e095ae7d8e643f279e27f56603bcd63c1d80e564
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-09-07 17:21:10 -0700

    Time: 58 ms (34.97%), Space: 13.8 MB (70.70%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        q = deque()
+        q.append(root)
+        rsv = []
+        
+        while q:
+            levelsize = len(q)
+            temp = []
+            for _ in range(levelsize):
+                node = q.popleft()
+                temp.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            rsv.append(temp[-1])
+        return rsv
