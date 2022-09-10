1829ad519ff41f14ea340344041c5828ead62a13
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-09-09 17:39:01 -0700

    Time: 58 ms (18.65%), Space: 13.8 MB (62.30%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.py
@@ -0,0 +1,13 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        lo, hi = 1, n
+        while lo < hi:
+            m = lo + (hi - lo) // 2
+            if isBadVersion(m):
+                hi = m
+            else:
+                lo = m + 1
+        return lo
