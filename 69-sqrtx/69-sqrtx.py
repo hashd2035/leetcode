6c1ac5e21b33b4d9e32294cef4cff936320fc28f
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-09-09 19:06:08 -0700

    Time: 39 ms (92.42%), Space: 13.9 MB (11.05%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,23 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x <= 1: 
+            return x
+        
+        lo, hi = 0, x
+        while lo < hi:
+            m = lo + (hi - lo) //2
+            
+            # if the condition is true, discard
+        #     if m * m == x:
+        #         return m
+        #     elif m * m > x: 
+        #         hi = m
+        #     else:
+        #         lo = m + 1
+        # return lo - 1
+
+            if m * m <= x:
+                lo = m + 1
+            else:
+                hi = m
+        return lo - 1            
