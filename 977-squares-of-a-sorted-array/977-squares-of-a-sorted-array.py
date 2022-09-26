ededc410c096b2c199944a15cab97e6659772cdf
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-09-25 19:15:31 -0700

    Time: 241 ms (89.33%), Space: 16.2 MB (79.72%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,6 +1,32 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        ans = []
+        neg, pos = [], []
         for n in nums:
-            ans.append(n*n)
-        return sorted(ans)
+            if n < 0:
+                neg.append(n*n)
+            else:
+                pos.append(n*n)
+        
+        neg.reverse()
+        
+        i, j, k = 0, 0, 0
+        while i < len(neg) and j < len(pos):
+            if neg[i] <= pos[j]:
+                nums[k] = neg[i]
+                i += 1 
+            elif j < len(pos):
+                nums[k] = pos[j]
+                j += 1 
+            k += 1
+
+        while i < len(neg):
+            nums[k] = neg[i]
+            i += 1 
+            k += 1            
+            
+        while j < len(pos):
+            nums[k] = pos[j]
+            j += 1 
+            k += 1            
+
+        return nums
