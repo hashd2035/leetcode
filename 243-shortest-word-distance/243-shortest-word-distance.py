36946c99861876fe16c08e4f882e7d401e71b786
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-09-25 17:55:06 -0700

    Time: 75 ms (85.12%), Space: 17.8 MB (47.39%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.py
+++ 243-shortest-word-distance/243-shortest-word-distance.py
@@ -1,47 +1,27 @@
 class Solution:
-    """
-    First Try failed the test case 
-    1. Attempted to try with different approach
-    2. Should have fixed the code - the approach was right    
-    - Lesson: 
-        (1) Come up with test (edge) cases before start coding or even approach the problem
-        (2) Concentrate - Always makes stupid mistake
-            This time, the line 19, 20 was missing
-    """
-    def firstTry(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        """
-        Clarification
-        - word1 != word2
-        - duplcate words allowed in wordDict
-        - guaranteed that word1, word2 exist in wordDict
-        """
-        index1, index2 = -1, -1
-        minDistance = math.inf
-        for i, word in enumerate(wordsDict):
-            if word == word1:
-                index1 = i
-            elif word == word2:
-                index2 = i
-            if index1 != -1 and index2 != -1:
-                minDistance = min(minDistance, abs(index1 - index2))
-        return minDistance
-    
-    """ Possible other approaches
-    - find all distances and find min diff
-    - find a, b pairs, then b, a pairs with left/right pointers, then, find min
-    - Sliding Window?
-    - At the end... There's no elegant solution, so, just solve bruteforce
-        And, coming up with bruteforce wasn't easy for me. Needs more practice
-    """
-    def naive(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        minDistance = math.inf
-        for i in range(len(wordsDict)):
-            if wordsDict[i] == word1:
-                for j in range(len(wordsDict)):
-                    if wordsDict[j] == word2:
-                        minDistance = min(minDistance, abs(i - j))
-        
-        return minDistance
-            
     def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        return self.firstTry(wordsDict, word1, word2)
+        """
+   i =    _
+        acab a, b
+        wordq = [2]
+        wordList = [a, b]
+        shortest = 
+        """
+        wordq = deque()
+        wordList = [word1, word2]
+        shortest = math.inf
+        for i, word in enumerate(wordsDict):
+            if word in wordList:
+                if len(wordq) == 2:
+                    shortest = min(shortest, abs(wordq[0]-wordq[1]))
+                    wordq.popleft()
+                if len(wordq) == 1:
+                    if wordsDict[wordq[0]] == word:
+                        wordq[0] = i
+                        continue
+                        
+                wordq.append(i)
+        if len(wordq) == 2:
+            shortest = min(shortest, abs(wordq[0]-wordq[1]))
+        return shortest        
+            
