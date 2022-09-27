bbb21da3133eefb2cc0440ffb1514512971460f8
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-09-26 17:44:46 -0700

    Time: 479 ms, Memory: 26 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hs = set()
+        for n in nums:
+            if n in hs:
+                return True
+            hs.add(n)
+        return False
+    
+    """
+    1231
+       _
+    123
+    """
