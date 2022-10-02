42295af79738a49e4c11689fe5c56ef4e346c85c
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-10-01 23:55:14 -0700

    Time: 998 ms (36.90%), Space: 15.4 MB (97.01%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,26 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        n = len(piles)
-        if n > h:
-            return -1
+        """
+        # how many and at most a pile
+        k = speed = 1..max(piles) 
         
-        def canEatAllBananas(speed):
-            hours = 0
-            for pile in piles:
-                hours += ceil(pile / speed)    
-            return hours if hours <= h else 0
-                    
-        piles.sort()
-        lo, hi = 1, piles[-1]
+        within h, but, max 
+        
+        curspeed = ceil(pile[i]/k)
+        """
+        minspeed = math.inf 
+        lo, hi = 1, max(piles)+1
 
         while lo < hi:
-            m = lo + (hi - lo) // 2
-            if canEatAllBananas(m):
-                hi = m
-            else: 
-                lo = m + 1
-        return hi
-            
-        
+            curspeed = lo + (hi - lo) // 2
+            hours = sum([math.ceil(p/curspeed) for p in piles])            
+
+            if hours <= h:
+                minspeed = min(minspeed, curspeed)
+                hi = curspeed
+            else:
+                lo = curspeed + 1
+        return minspeed
                 
-        
