32cf686960c09e3e0962a9854c2371570fa7c6b4
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-10-02 00:06:53 -0700

    Time: 84 ms (47.76%), Space: 13.9 MB (71.54%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
         #39
-        def bisect_left(a, target, lo, hi):
+        def bisect(target, lo, hi):
             while lo < hi:
                 m = lo + (hi - lo) // 2
-                if target <= a[m]:
+                if target <= nums[m]:
                     hi = m
                 else:
                     lo = m + 1
@@ -14,7 +14,7 @@ class Solution:
         nums.sort()
         max_sum = -1
         for i in range(n):
-            j = bisect_left(nums, k-nums[i], i+1, n)
+            j = bisect(k-nums[i], i+1, n)
             if i < j:
                 max_sum = max(max_sum, nums[i]+nums[j])
         return max_sum
