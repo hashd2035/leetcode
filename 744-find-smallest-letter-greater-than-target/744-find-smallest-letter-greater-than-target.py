fa56e413da4a0da7c1a75cdb2aa4fbe440a0855c
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-10-02 13:43:06 -0700

    Time: 147 ms (73.92%), Space: 14.3 MB (90.48%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,11 +1,5 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        """
-        
-        """
-        # if target >= letters[-1]:
-        #     letters[0]
-        
         lo, hi = 0, len(letters)
         while lo < hi:
             m = lo + (hi - lo) // 2
@@ -14,8 +8,4 @@ class Solution:
             else:
                 lo = lo + 1
                 
-        
-        if lo == len(letters):
-            return letters[0]
-        else:
-            return letters[lo]
+        return letters[0] if lo == len(letters) else letters[lo]
