54d513494467ff248baa879eeaec64996b0691ff
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-10-13 17:39:44 -0700

    Time: 38 ms (83.20%), Space: 14.1 MB (5.04%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -75,38 +75,22 @@ class Solution:
         
     def approach3(self, matrix: List[List[int]]) -> List[int]:
         self.DIRECTIONS = [(0,1), (1,0), (0,-1), (-1,0)] # right, down, left, up
-        m, n = len(matrix), len(matrix[0])
         self.ans, visited = [matrix[0][0]], set()
-                
+        m, n = len(matrix), len(matrix[0])                
+        
         def dfs(row, col):            
             visited.add((row, col))
             for dr, dc in self.DIRECTIONS:
                 r, c = row + dr, col + dc
                 if 0 <= r < m and 0 <= c < n and (r, c) not in visited:
-                    self.ans.append(matrix[row][col])
+                    self.ans.append(matrix[r][c])
                     dfs(r, c)
-            self.DIRECTIONS = self.DIRECTIONS[1:] + self.DIRECTIONS[:1]
+                    
+                # Rotate directions!!
+                self.DIRECTIONS = self.DIRECTIONS[1:] + self.DIRECTIONS[:1]
             
-        dfs(0, 0)
-        return ans
-    
-    
-    def anser(self, matrix):
-        self.dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited = set()
-        self.ans = [matrix[0][0]]
-        def dfs(row, col):
-            visited.add((row, col))
-            for r, c in self.dirs:
-                new_r = r + row
-                new_c = c + col
-                if 0 <= new_r < len(matrix) and \
-                0 <= new_c < len(matrix[0]) and (new_r, new_c) not in visited:
-                    self.ans.append(matrix[new_r][new_c])
-                    dfs(new_r, new_c)
-                self.dirs = self.dirs[1:] + self.dirs[:1]
         dfs(0, 0)
         return self.ans
         
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        return self.anser(matrix)
+        return self.approach3(matrix)
