042eb177896b3d38ea4b7afba9cd69d579b9dd5b
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-11-02 15:26:53 -0700

    Time: 39 ms (93.79%), Space: 14.1 MB (49.78%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def cascade(self, nums: List[int]) -> List[List[int]]:
+        subsets = [[]] 
+        s, e, prev = 0, 0, None
+        
+        for n in sorted(nums):
+            if prev:
+                s = 0 if prev != n else e + 1
+            e = len(subsets) - 1
+            
+            for i in range(s, e + 1):
+                subsets.append(subsets[i] + [n])
+            
+            prev = n
+            
+        return subsets
+    
+    def incrementalLength(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, track):
+            subsets.append(track)
+            
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                backtrack(i+1, track + [nums[i]])
+        
+        nums.sort()
+        subsets = []
+        backtrack(0, [])
+        return subsets
+                     
+                     
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        return self.incrementalLength(nums)
