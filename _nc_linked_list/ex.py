7bbbb4e2f3863bfe472ba6f96d3296d0b68bf9ca
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-11-17 20:05:53 +0000

    cleaned up a bit

---
+++ _nc_linked_list/ex.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+"""
+Easy
+0206-reverse-linked-list 
+"""
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:        
+
+"""
+Easy
+0206-reverse-linked-list
+"""
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+"""
+Medium
+0143-reorder-list
+"""
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+"""
+Medium
+0019-remove-nth-node-from-end-of-list
+"""
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+"""
+Medium Walmart
+138. Copy List with Random Pointer
+"""
+# Definition for a Random Node.
+class RNode:
+    def __init__(self, x: int, next: 'RNode' = None, random: 'RNode' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[RNode]') -> 'Optional[RNode]':
+
+"""
+Medium
+2. Add Two Numbers
+"""
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+
+"""
+Easy
+0141-linked-list-cycle 
+"""
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
