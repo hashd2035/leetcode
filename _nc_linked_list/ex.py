7fd9890072838e6c97f7cb9e2cdb49eb45ebbf47
hashd2035 <94868784+hashd2035@users.noreply.github.com>
2022-11-18 05:44:34 +0000

    update the skeleton

--- _nc_linked_list/ex.py
+++ _nc_linked_list/ex.py
@@ -10,16 +10,7 @@ Easy
 """
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:        
-        prev, cur = None, head 
-        while cur:
-            # reverse
-            temp = cur.next 
-            cur.next = prev
-
-            # shift
-            prev, cur = cur, temp 
-
-        return prev
+      
 
 """
 Easy
@@ -27,30 +18,6 @@ Easy
 """
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-
-        if list1.value <= list2.value:
-            dummy.next = list1 
-            list1 = list1.next 
-        else:
-            dummy.next = list2
-            list2 = list2.next 
-
-        tail = dummy.next 
-
-        while list1 and list2:
-            if list1.val <= list2.val:
-                tail.next = list1
-                list1 = list1.next 
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next 
-
-        if list1: tail.next = list1
-        if list2: tail.next = list2
-
-        return dummy.next 
         
 """
 Medium
